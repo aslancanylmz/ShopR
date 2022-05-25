9327312faa2b50f212e32afe5ed7b242d08b9087
Aslancan Yılmaz <aslancanylmz@gmail.com>
2022-05-26 01:06:53 +0300

    Designed bottom tab bar and added icons

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -3,6 +3,7 @@ export const COLORS = {
   black: '#000',
   textInputBorderColor: '#B7B1B1',
   ocher: '#D2A670',
+  trendyol: '#ff7f00',
   invalidColor: 'red',
   successColor: 'green'
 };
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -3,14 +3,50 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import Cart from '../screens/Cart';
 import Products from '../screens/Products';
+import { COLORS } from '../constants/theme';
+import { Icon, iconNames } from '../components/Icon';
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Products' component={Products} />
-      <Tab.Screen name='Cart' component={Cart} />
+    <Tab.Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarActiveTintColor: COLORS.trendyol }}>
+      <Tab.Screen
+        name='Home'
+        component={Cart}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Icon iconName={iconNames.Home} color={focused ? COLORS.trendyol : COLORS.textInputBorderColor}></Icon>
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Products'
+        component={Products}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Icon iconName={iconNames.Profile} color={focused ? COLORS.trendyol : COLORS.textInputBorderColor}></Icon>
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Cart'
+        component={Cart}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Icon iconName={iconNames.Cart} color={focused ? COLORS.trendyol : COLORS.textInputBorderColor}></Icon>
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Coupon'
+        component={Cart}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Icon iconName={iconNames.Coupon} color={focused ? COLORS.trendyol : COLORS.textInputBorderColor}></Icon>
+          )
+        }}
+      />
     </Tab.Navigator>
   );
 };
