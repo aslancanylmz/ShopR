44d742c42f9798fcb1283dcbae1f9d46bc1624d5
Aslancan Yılmaz <aslancanylmz@gmail.com>
2022-06-07 23:30:42 +0300

    Add collapsible package

---
+++ src/components/CollapsibleButton/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  collapsibleButtonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 12,
+    paddingHorizontal: 12
+  }
+});
+
+export default styles;
